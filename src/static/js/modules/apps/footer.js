da5a8737f1f60da953b510f6f465dfeffc18aa4a
bessavagner <bessavagner@gmail.com>
2025-08-07 15:16:08 -0300

    fix: add institucional link to footer

--- src/static/js/modules/apps/footer.js
+++ src/static/js/modules/apps/footer.js
@@ -3,44 +3,60 @@
 import { AnchorToSection } from "../components/actions.js";
 import { Component } from "../engine/core.js";
 
-
 export class AppFooter extends Component {
-    constructor() {
-        super("footer", "bg-base-100 border-t py-6");
-        this.container = new Component(
-            "div",
-            "container mx-auto px-4 flex flex-col md:flex-row items-center justify-between"
-        );
-        this.year = new Date().getFullYear();
-    }
-    renderContent() {
-        this.container.render({ target: this.element });
+  constructor() {
+    super("footer", "bg-base-100 border-t py-6");
+    this.container = new Component(
+      "div",
+      "container mx-auto px-4 flex flex-col md:flex-row items-center justify-between"
+    );
+    this.year = new Date().getFullYear();
+  }
+  renderContent() {
+    this.container.render({ target: this.element });
 
-        new Component("p", "text-sm text-gray-500")
-            .setText(`© ${this.year} Box Base Inicial. Todos os direitos reservados.`)
-            .render({ target: this.container.element });
+    new Component("p", "text-sm text-gray-500")
+      .setText(`© ${this.year} Box Base Inicial. Todos os direitos reservados.`)
+      .render({ target: this.container.element });
 
-        const links = [
-            { sectionTargetId: "testimonials-section", text: "Depoimentos"},
-            { sectionTargetId: "coaches-section", text: "Equipe"},
-            { sectionTargetId: "locationcontact-section", text: "Contato"},
-            { sectionTargetId: "gallery-section", text: "Galeria" },
-        ];
+    const links = [
+      {
+        sectionTargetId: "testimonials-section",
+        href: "/#testimonials-section",
+        text: "Depoimentos",
+      },
+      {
+        sectionTargetId: "coaches-section",
+        href: "/#coaches-section",
+        text: "Equipe",
+      },
+      {
+        sectionTargetId: "locationcontact-section",
+        href: "/#locationcontact-section",
+        text: "Contato",
+      },
+      {
+        sectionTargetId: "gallery-section",
+        href: "/#gallery-section",
+        text: "Galeria",
+      },
+      { href: "/institucional/", text: "Institutional" },
+    ];
 
-        const menu = new Component("ul", "flex space-x-4");
-        links.forEach((item) => {
-            new AnchorToSection({})
-            .renderContent(item)
-            .setStyle({
-                cursor: "pointer",
-            })
-            .render({ target: menu.element });
-        });
+    const menu = new Component("ul", "flex space-x-4");
+    links.forEach((item) => {
+      new AnchorToSection({})
+        .renderContent(item)
+        .setStyle({
+          cursor: "pointer",
+        })
+        .render({ target: menu.element });
+    });
 
-        menu.render({ target: this.container.element });
-    }
-    init(target) {
-        this.renderContent();
-        return this.render({ target });
-    }
+    menu.render({ target: this.container.element });
+  }
+  init(target) {
+    this.renderContent();
+    return this.render({ target });
+  }
 }
